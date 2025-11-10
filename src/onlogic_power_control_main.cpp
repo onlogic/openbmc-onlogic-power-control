53c5940c5d5c7c7313de0776a83a610196ab6630
Justin Seely <justin.seely@onlogic.com>
2025-11-10 10:47:57 -0500

    Mask DBus Interface Properties to provide hooks (#5)

--- src/onlogic_power_control_main.cpp
+++ src/onlogic_power_control_main.cpp
@@ -1,11 +1,16 @@
-#include <org/openbmc/control/Power/server.hpp>
 #include <phosphor-logging/lg2.hpp>
+
 #include <sdbusplus/server.hpp>
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+
 #include <boost/asio/io_context.hpp>
-#include <xyz/openbmc_project/State/Chassis/server.hpp>
-#include <xyz/openbmc_project/State/Host/server.hpp>
+
+// DBus interface includes
+#include <org/openbmc/control/Power/server.hpp>         // Generated from src/yaml/org/openbmc/control/Power.interface.yaml
+#include <xyz/openbmc_project/State/Chassis/server.hpp> // Generated from subprojects/phosphor-dbus-interfaces/yaml/xyz/openbmc_project/State/Chassis.interface.yaml
+#include <xyz/openbmc_project/State/Host/server.hpp>    // Generated from subprojects/phosphor-dbus-interfaces/yaml/xyz/openbmc_project/State/Host.interface.yaml
+#include <xyz/openbmc_project/Common/error.hpp>         // Generated from subprojects/phosphor-dbus-interfaces not sure the exact yaml file
 
 PHOSPHOR_LOG2_USING;
 
@@ -41,10 +46,11 @@ protected:
 class Power : ObjectServer<sdbusplus::server::org::openbmc::control::Power>
 {
 public:
-    Power(sdbusplus::bus_t& bus, const std::string& node)
-        : ObjectServer(bus, sdbusplus::server::org::openbmc::control::Power::instance_path, sdbusplus::server::org::openbmc::control::Power::default_service, node)
+    Power(std::shared_ptr<sdbusplus::asio::connection> conn, const std::string& node)
+        : ObjectServer(*conn, sdbusplus::server::org::openbmc::control::Power::instance_path, sdbusplus::server::org::openbmc::control::Power::default_service, node)
     {
-        // TODO(BMC-32): Have BMC receive pgood signal from sequence MCU
+        // Initially signal that power is good
+        // We don't really care about this signal it just needs to be high for BMC::Ready
         pgood(1);
     };
 };
@@ -52,17 +58,93 @@ public:
 class Chassis : ObjectServer<sdbusplus::server::xyz::openbmc_project::state::Chassis>
 {
 public:
-    Chassis(sdbusplus::bus_t& bus, const std::string& node)
-        : ObjectServer(bus, getPath(node).c_str(), Chassis::default_service, node)
+    Chassis(std::shared_ptr<sdbusplus::asio::connection> conn, const std::string& node)
+        : ObjectServer(*conn, getPath(node).c_str(), Chassis::default_service, node), node(node)
     {
-        requestedPowerTransition(Transition::On);
-        currentPowerState(PowerState::On);
-        currentPowerStatus(PowerStatus::Good);
-        lastStateChangeTime(getCurrentTimeMs());
+        determineInitialState();
     };
 
+private:
+    const std::string node;
+
+public:
+
+    /// @brief Sets the requested power transition state of the chassis
+    /// @param value - Can be one of Off, On, or PowerCycle
+    /// @return 
+    Transition requestedPowerTransition(Transition value)
+    {
+        info("Chassis{NODE}: Requested power transition from {OLD} to {NEW}", "NODE", node, "OLD", sdbusplus::server::xyz::openbmc_project::state::Chassis::requestedPowerTransition(), "NEW", value);
+        return sdbusplus::server::xyz::openbmc_project::state::Chassis::requestedPowerTransition(value);
+    }
+
+    /// @brief Gets the requested power transition state of the chassis
+    /// @return - The requested power transition state: Off, On, or PowerCycle
+    Transition requestedPowerTransition() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Chassis::requestedPowerTransition();
+    }
+
+    /// @brief Sets the current power state of the chassis
+    /// @param value - Can be one of Off, On, TransitioningToOff, TransitioningToOn
+    /// @return - The updated power state
+    PowerState currentPowerState(PowerState value)
+    {
+        info("Chassis{NODE}: Current power state change to {NEW}", "NODE", node, "NEW", value);
+        sdbusplus::server::xyz::openbmc_project::state::Chassis::currentPowerState(value);
+        lastStateChangeTime(getCurrentTimeMs());
+        return value;
+    }
+
+    /// @brief Gets the current power state of the chassis
+    /// @return - The current power state of the chassis: Off, On, TransitioningToOff, TransitioningToOn
+    PowerState currentPowerState() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Chassis::currentPowerState();
+    }
+
+    /// @brief Sets the current power status of the chassis (Should only be Good, Brownout or Undefined based on what information we can get from sequence MCU)
+    /// @param value - Can be one of Good, Brownout, UninterruptiblePowerSupply, Undefined
+    /// @return - The updated power status
+    PowerStatus currentPowerStatus(PowerStatus value)
+    {
+        // Noop hook: add logic here if needed
+        info("Chassis{NODE}: Current power status change to {NEW}", "NODE", node, "NEW", value);
+        sdbusplus::server::xyz::openbmc_project::state::Chassis::currentPowerStatus(value);
+        lastStateChangeTime(getCurrentTimeMs());
+        return value;
+    }
+
+    /// @brief Gets the current power status of the chassis
+    /// @return - The current power status of the chassis: Good, Brownout, UninterruptiblePowerSupply, Undefined
+    PowerStatus currentPowerStatus() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Chassis::currentPowerStatus();
+    }
+
+private:
+    /// @brief Interogates the system to determine the initial chassis state on BMC startup
+    void determineInitialState()
+    {
+        // We will just always assume Power Good if the BMC has power for now. 
+        // We can improve this later by including brownout detection and UPS status when available from the sequence MCU
+        currentPowerStatus(PowerStatus::Good);
+
+        // TODO(BMC-19): currentPowerState should be determined by querying the sequence MCU for the actual chassis power state
+        currentPowerState(PowerState::On);
+
+        // TODO(BMC-19): requestedPowerTransition should match the currentPowerState on BMC startup
+        requestedPowerTransition(Transition::On);
+
+        lastStateChangeTime(getCurrentTimeMs());
+    }
+
+public:
     static constexpr const char* default_service = "xyz.openbmc_project.State.Chassis";
 
+    /// @brief Constructs the DBus object path for the chassis based on the node
+    /// @param node - The node identifier, currently this should always be "0"
+    /// @return - The DBus object path for the chassis
     static std::string getPath(const std::string& node)
     {
         return std::string(Chassis::namespace_path::value) + "/" + std::string(Chassis::namespace_path::chassis) + node;
@@ -72,15 +154,81 @@ public:
 class Host : ObjectServer<sdbusplus::server::xyz::openbmc_project::state::Host>
 {
 public:
-    Host(sdbusplus::bus_t& bus, const std::string& node)
-        : ObjectServer(bus, getPath(node).c_str(), Host::default_service, node)
+    Host(std::shared_ptr<sdbusplus::asio::connection> conn, const std::string& node)
+        : ObjectServer(*conn, getPath(node).c_str(), Host::default_service, node), node_(node)
     {
-        requestedHostTransition(Transition::On);
+        determineInitialState();
+
+        // TODO(BMC-15): For Titanium and Tacton SMBus communication to the sequence MCU is isolated below S0
+        // so we can only support Transition::Reboot
         allowedHostTransitions({Transition::On, Transition::Off, Transition::Reboot});
-        currentHostState(HostState::Running);
-        restartCause(RestartCause::Unknown);
     };
 
+private:
+    const std::string node_;
+
+public:
+    /// @brief Sets the requested host transition
+    /// @param value - Can be one of Off, On, Reboot, GracefulWarmReboot, ForceWarmReboot
+    /// @return - The updated requested host transition
+    Transition requestedHostTransition(Transition value)
+    {
+        
+        // Noop hook: add logic here if needed
+        info("Host{NODE}: Requested host transition from {OLD} to {NEW}", "NODE", node_, "OLD", sdbusplus::server::xyz::openbmc_project::state::Host::requestedHostTransition(), "NEW", value);
+        sdbusplus::server::xyz::openbmc_project::state::Host::requestedHostTransition(value);
+        return value;
+    }
+
+    /// @brief Gets the requested host transition
+    /// @return - The requested host transition: Off, On, Reboot, GracefulWarmReboot, ForceWarmReboot
+    Transition requestedHostTransition() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Host::requestedHostTransition();
+    }
+
+    /// @brief Sets the current host state
+    /// @param value - The new current host state
+    /// @return - The updated current host state
+    HostState currentHostState(HostState value)
+    {
+        info("Host{NODE}: Current host state change to {NEW}", "NODE", node_, "NEW", value);
+        sdbusplus::server::xyz::openbmc_project::state::Host::currentHostState(value);
+        return value;
+    }
+
+    HostState currentHostState() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Host::currentHostState();
+    }
+
+    RestartCause restartCause(RestartCause value)
+    {
+        // Noop hook: add logic here if needed
+        info("Host{NODE}: Restart cause change to {NEW}", "NODE", node_, "NEW", value);
+        sdbusplus::server::xyz::openbmc_project::state::Host::restartCause(value);
+        return value;
+    }
+
+    RestartCause restartCause() const
+    {
+        return sdbusplus::server::xyz::openbmc_project::state::Host::restartCause();
+    }
+
+private:
+    /// @brief Interogates the system to determine the initial host state on BMC startup
+    void determineInitialState()
+    {
+        // TODO(BMC-19): currentHostState should be determined by querying the sequence MCU for the actual host state
+        currentHostState(HostState::Running);
+        // TODO(BMC-19): requestedHostTransition should match the currentHostState on BMC startup
+        requestedHostTransition(Transition::On);
+
+        // TODO(BMC-80): restartCause should be determined by querying the sequence MCU or BIOS for the actual restart cause
+        restartCause(RestartCause::Unknown);
+    }
+
+public:
     static constexpr const char* default_service = "xyz.openbmc_project.State.Host";
 
     static std::string getPath(const std::string& node)
@@ -103,9 +251,9 @@ int main(int argc, char* argv[]) {
     boost::asio::io_context io;
     auto conn = std::make_shared<sdbusplus::asio::connection>(io);
 
-    Power power0(*conn, node);
-    Chassis chassis0(*conn, node);
-    Host host0(*conn, node);
+    Power power0(conn, node);
+    Chassis chassis0(conn, node);
+    Host host0(conn, node);
 
     io.run();
 }
